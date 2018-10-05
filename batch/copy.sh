5c97ea6b8dbfbc89c66d71e884280bb3432b0ce9
12X4035 <daichi.worldtrip.4.15@gmail.com>
2018-10-05 17:12:47 +0900

    Update copy.sh

--- batch/copy.sh
+++ batch/copy.sh
@@ -4,8 +4,8 @@ do
 	   	echo ${f%.*}
 	for i in *;
 		do
-			export PGPASSWORD=G7uz3T8p;
-			"/Applications/Postgres.app/Contents/Versions/9.6/bin/psql"-p 3432 -U tick_db -h rm-2in2pw90m04p7vwhvgo.pg.rds.aliyuncs.com -d tick -c "CREATE TABLE $i (SELECT * FROM after15 WHERE 1=2);"
+			export PGPASSWORD=*******;
+			"/Applications/Postgres.app/Contents/Versions/9.6/bin/psql"-p 3432 -U tick_db -h *************** -d tick -c "CREATE TABLE $i (SELECT * FROM after15 WHERE 1=2);"
 		done
 done
 
